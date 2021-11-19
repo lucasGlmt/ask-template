82a3f6e81f6c2cd962c5d7c1a89361b42d628c61
lucas.guilleminot <lcs.glmt@gmail.com>
2021-11-19 15:03:36 +0100

    Formatage

--- back/models/Answer.js
+++ back/models/Answer.js
@@ -1,8 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
-  const Answer = sequelize.define('Answer', {
-    answer: DataTypes.STRING
-  }, {});
-  Answer.associate = function(models) {
+  const Answer = sequelize.define(
+    'Answer',
+    {
+      answer: DataTypes.STRING,
+    },
+    {}
+  );
+  Answer.associate = function (models) {
     models.Answer.belongsTo(models.Question);
   };
   return Answer;
